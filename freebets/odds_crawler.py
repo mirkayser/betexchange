82ffb925f80a34de7bd2c2fc7e827f162297b438
Mirko <mirko.scherer@gmx.net>
2016-08-08 18:23:51 -0400

    modified odds_crawler

--- freebets/odds_crawler.py
+++ freebets/odds_crawler.py
@@ -11,17 +11,22 @@ from optparse import OptionParser
 from operator import itemgetter
 from my.spider import Spider
 from my.tools import *
+from pyik.performance import cached
 
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By		
 
-def get_data_sportsbooks(spider):
+@cached
+def get_data_sportsbooks():
 	
 	urls = [	'http://www.oddschecker.com/olympics/tennis/olympic-mens',
 						'http://www.oddschecker.com/olympics/tennis/olympic-womens',
 						'http://www.oddschecker.com/olympics/tennis/olympic-specials',
-						'http://www.oddschecker.com/olympics/tennis/olympic-womens-doubles'
+						'http://www.oddschecker.com/olympics/tennis/olympic-womens-doubles',
+						'http://www.oddschecker.com/tennis/atp-los-cabos'
 						]
+	
+	spider = Spider(gui=0)
 	
 	#get links
 	matchlinks=[]
@@ -46,18 +51,28 @@ def get_data_sportsbooks(spider):
 			elems =  player.find_elements_by_xpath('./td')
 			dic['name'] = elems[0].get_attribute('textContent')
 			
+			split = dic['name'].split("/")
+			if len( split )>1:
+				dic['name'] = split[0].split(" ")[-1]+'/'+split[1].split(" ")[-1]
+					
 			bet365 = elems[1].get_attribute('data-odig')
 			if bet365!=None: 
 				dic['bet365']  = float(bet365)
 				sportsbook.append(dic)
 	
+	spider.close()
+	
 	return sportsbook
 	
-def get_data_exchange(spider):
-
+@cached	
+def get_data_exchange():
+	
+	spider = Spider(gui=0)
+	
 	#get data from betfair exchange
 	print 'get data from betfair exchange:'
-	spider.get_url('https://www.betfair.com/exchange/tennis/competition?id=9574834')
+	#~ spider.get_url('https://www.betfair.com/exchange/tennis/competition?id=9574834')
+	spider.get_url('https://www.betfair.com/exchange/tennis')
 	
 	markets = spider.driver.find_elements_by_xpath('//div[@class="container-market"]')
 	
@@ -71,27 +86,31 @@ def get_data_exchange(spider):
 		else:
 			
 			name1,name2 = text
-			price1 = market.find_element_by_xpath('./div/ul/li/ul/li[contains(@class,"lay selection-0")]/button').get_attribute("textContent")
-			price2 = market.find_element_by_xpath('./div/ul/li/ul/li[contains(@class,"lay selection-1")]/button').get_attribute("textContent")
-		
-			price1,price2 = float(price1),float(price2)
-		
-			names.append(name1)
-			names.append(name2)
-			prices.append(price1)
-			prices.append(price2)
+			price1 = market.find_element_by_xpath('./div/ul/li/ul/li[contains(@class,"lay selection-0")]/button').text
+			price2 = market.find_element_by_xpath('./div/ul/li/ul/li[contains(@class,"lay selection-1")]/button').text
+			
+			if price1!=' ':
+				price1 = float(price1)
+				names.append(name1)
+				prices.append(price1)
+			if price2!=' ':
+				price2 = float(price2)
+				names.append(name2)
+				prices.append(price2)
+	
+	spider.close()
 	
 	return names,prices
 
 def compare(data,book='bet365',limit=0.5):
 			
-	out=''
+	out='\nResult:\n'
 				
 	for item in data:
 		
 		if item[book] < 1.5: continue
 		
-		diff = np.abs( item[book] - item['lay'] )
+		diff = item['lay'] - item[book]
 		if diff > limit: continue
 		
 		out += '%.2f  %s  (back=%.2f, lay=%.2f)\n' % (diff,item['name'].ljust(20," "),item[book],item['lay'])
@@ -103,22 +122,26 @@ def compare(data,book='bet365',limit=0.5):
 def main():
 	print 'here starts main program'
 	
-spider = Spider(gui=0)
 
-sportsbook = get_data_sportsbooks(spider)
-names,prices = get_data_exchange(spider)
 
-spider.close()	
-	
+sportsbook = get_data_sportsbooks()
+names,prices = get_data_exchange()
+
 #join datasets
 print 'join datasets'
 data = []
 for dic in sportsbook:
+	found=False
 	for i in xrange(len(names)):
 		if dic['name'] in names[i]:
 			dic['lay'] = prices[i] 
 			data.append(dic)
-
+			found=True
+			break
+	if not found:
+		print '--WARNING: not found (%s)' % dic['name']
+print '%d/%d matched' % (len(data),len(sportsbook))
+				
 compare(data,limit=0.5)
 	
 
