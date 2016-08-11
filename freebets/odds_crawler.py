2606d33cfa597f54d754b3c90caed79de0b2752d
Mirko <mirko.scherer@gmx.net>
2016-08-11 17:41:41 -0400

    added repeat functionality to odds_crawler

--- freebets/odds_crawler.py
+++ freebets/odds_crawler.py
@@ -3,7 +3,7 @@
 from IPython import embed
 #embed() # this call anywhere in your program will start IPython
 
-import sys, os, time, progressbar
+import sys, os, time,re, progressbar
 import datetime
 import pickle
 import numpy as np
@@ -12,6 +12,8 @@ from operator import itemgetter
 from my.spider import Spider
 from my.tools import *
 from pyik.performance import cached
+
+from betcalc import Bets
 
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By		
@@ -23,37 +25,50 @@ def get_data_sportsbooks():
 						'http://www.oddschecker.com/olympics/tennis/olympic-womens',
 						'http://www.oddschecker.com/olympics/tennis/olympic-specials',
 						'http://www.oddschecker.com/olympics/tennis/olympic-womens-doubles',
-						'http://www.oddschecker.com/tennis/atp-los-cabos'
+						'http://www.oddschecker.com/tennis/atp-los-cabos',
+						'http://www.oddschecker.com/tennis/itf-futures',
+						'http://www.oddschecker.com/tennis/challenger-tour'
 						]
+	market_names = [ 'rio men','rio women','rio men double','rio women double','atp cabo','itf futures','challenger' ]
 	
 	spider = Spider(gui=0)
 	
 	#get links
 	matchlinks=[]
-	for url in urls:
+	for i,url in enumerate(urls):
 		print 'get links from:  %s' % url
 		spider.get_url(url)
 		elems = spider.driver.find_elements_by_xpath('//td[@class="betting"]/a')
 		for elem in elems:
-			matchlinks.append(elem.get_attribute("href"))
+			if "in-play" in elem.get_attribute("class"): continue
+			else:
+				matchlinks.append( (elem.get_attribute("href"),market_names[i]) )
 	
 	#get data from oddschecker
 	sportsbook = []	
 	print 'getting data from sportsbooks: (%d events)' % len(matchlinks)
 	bar = progressbar.ProgressBar()
-	for link in bar(matchlinks):
+	for item in bar(matchlinks):
+		
+		link = item[0]
+		market_name=item[1]
+		
+		#skip handicaps
+		if "handicap" in link: continue
 		
 		spider.get_url(link)
 		players = spider.driver.find_elements_by_xpath('//tbody/tr')
 		
 		for player in players:
-			dic = {}
+			dic = { 'market-name':market_name }
 			elems =  player.find_elements_by_xpath('./td')
 			dic['name'] = elems[0].get_attribute('textContent')
 			
 			split = dic['name'].split("/")
 			if len( split )>1:
 				dic['name'] = split[0].split(" ")[-1]+'/'+split[1].split(" ")[-1]
+			else:
+				dic['name'] = dic['name'].split(" ")[-1]
 					
 			bet365 = elems[1].get_attribute('data-odig')
 			if bet365!=None: 
@@ -67,82 +82,166 @@ def get_data_sportsbooks():
 @cached	
 def get_data_exchange():
 	
-	spider = Spider(gui=0)
+	start='unknown'
+	spider = Spider(gui=0)	
 	
-	#get data from betfair exchange
-	print 'get data from betfair exchange:'
-	#~ spider.get_url('https://www.betfair.com/exchange/tennis/competition?id=9574834')
+	#get urls
+	urls,market_names=[],[]
 	spider.get_url('https://www.betfair.com/exchange/tennis')
+	elems = spider.driver.find_elements_by_xpath('//ul[@class="children"]/li/a')
+	for elem in elems:
+		url = elem.get_attribute("href")
+		market_name = elem.get_attribute("market-name")
+		urls.append(url)
+		market_names.append(market_name)	
 	
-	markets = spider.driver.find_elements_by_xpath('//div[@class="container-market"]')
+	print 'get data from betfair exchange: (%d urls)' % len(urls)
 	
-	names,prices = [],[]
-	bar = progressbar.ProgressBar()
-	for market in bar(markets):
+	names,prices,starts,mnames = [],[],[],[]
+	for j,url in enumerate(urls):
 		
-		#~ market = markets[5]
-		text = market.find_element_by_xpath('./a/div').text.split(" v ")
-		if len(text)!=2: continue
-		else:
+		print 'get data from market %d/%d: (%s)' % (j,len(urls),market_names[j])
 			
-			name1,name2 = text
-			price1 = market.find_element_by_xpath('./div/ul/li/ul/li[contains(@class,"lay selection-0")]/button').text
-			price2 = market.find_element_by_xpath('./div/ul/li/ul/li[contains(@class,"lay selection-1")]/button').text
+		spider.get_url(url)
+		
+		markets = spider.driver.find_elements_by_xpath('//div[@class="container-market"]')
+	
+		#skip empty markets
+		if len(markets)==0:
+			print '--WARNING: market empty (%s)' % market_names[j]
+			continue
+	
+		bar = progressbar.ProgressBar()
+		for market in bar(markets):
 			
-			if price1!=' ':
-				price1 = float(price1)
-				names.append(name1)
-				prices.append(price1)
-			if price2!=' ':
-				price2 = float(price2)
-				names.append(name2)
-				prices.append(price2)
+			#~ market = markets[5]
+			text = market.find_element_by_xpath('./a/div').text
+			
+			if len(text.split('\n'))>1:
+				text,start = text.split('\n')
+	
+			players = text.split(" v ")
+			if len(players)!=2: continue
+			else:
+				
+				for i in xrange(len(players)):
+					split = players[i].split("/")
+					if len(split)==2:
+						players[i] = split[0].rstrip().split(" ")[-1]+'/'+split[1].rstrip().split(" ")[-1]
+					else:
+						players[i] = players[i].split(" ")[-1]
+						
+				price1 = market.find_element_by_xpath('./div/ul/li/ul/li[contains(@class,"lay selection-0")]/button').text
+				price2 = market.find_element_by_xpath('./div/ul/li/ul/li[contains(@class,"lay selection-1")]/button').text
+				
+				if price1!=' ':
+					price1 = float(price1)
+					names.append(players[0])
+					prices.append(price1)
+					starts.append(start)
+					mnames.append(market_names[j])
+				if price2!=' ':
+					price2 = float(price2)
+					names.append(players[1])
+					prices.append(price2)
+					starts.append(start)
+					mnames.append(market_names[j])
 	
 	spider.close()
 	
-	return names,prices
+	return names,prices,starts,mnames
 
 def compare(data,book='bet365',limit=0.5):
+	
+	success=False
 			
 	out='\nResult:\n'
 				
 	for item in data:
 		
-		if item[book] < 1.5: continue
-		
 		diff = item['lay'] - item[book]
+		
+		if item[book]<1.5 and diff>0: continue
 		if diff > limit: continue
 		
-		out += '%.2f  %s  (back=%.2f, lay=%.2f)\n' % (diff,item['name'].ljust(20," "),item[book],item['lay'])
-
+		out += '%5.2f  %s  back=%5.2f  lay=%5.2f  -> %s  (%s)\n' % (diff,item['name'].ljust(20," "),item[book],item['lay'],item['start'],item['market-name'])
+		
+		if diff<0:
+			success=True
+		
 	if out=='': out = 'WARNING: no item fits specifications'
 	
 	print out
+	
+	return success
 		
 def main():
-	print 'here starts main program'
+	print ''
 	
+	sportsbook = get_data_sportsbooks()
+	names,prices,starts,mnames = get_data_exchange()
+	
+	#join datasets
+	print 'join datasets'
+	data = []
+	for dic in sportsbook:
+		found=False
+		for i in xrange(len(names)):
+			keyword = dic['name']
+			if re.search(keyword,names[i],re.IGNORECASE):
+			#~ if dic['name'] in names[i]:
+			#~ if names[i] in dic['name']:
+			
+				dic['lay'] = prices[i] 
+				dic['start'] = starts[i]
+				dic['market-name'] = mnames[i]
+				data.append(dic)
+				found=True
+				break
+		#~ if not found:
+			#~ print '--WARNING: not found %s (%s)' % (dic['name'],dic['market-name'])
+	print '%d/%d matched' % (len(data),len(sportsbook))
+					
+	success = compare(data,limit=0.2)
+
+	return success
 
 
-sportsbook = get_data_sportsbooks()
-names,prices = get_data_exchange()
+if __name__ == "__main__":
 
-#join datasets
-print 'join datasets'
-data = []
-for dic in sportsbook:
-	found=False
-	for i in xrange(len(names)):
-		if dic['name'] in names[i]:
-			dic['lay'] = prices[i] 
-			data.append(dic)
-			found=True
-			break
-	if not found:
-		print '--WARNING: not found (%s)' % dic['name']
-print '%d/%d matched' % (len(data),len(sportsbook))
+	#parse options
+	parser = OptionParser()
+	parser.add_option("-r","--repeat", dest="repeat", default=False,
+	                  help="repeat x times")
+	(options, args) = parser.parse_args()
+
+	if not options.repeat:
+		success = main()	
+		embed()
+		
+	else:
+		count=0
+		while count<int(options.repeat):
+			
+			print 'scrape %d/%d' %(count+1,int(options.repeat))
+			os.system("rm cache-*")
+			
+			success = main()
+			
+			if success: 
 				
-compare(data,limit=0.5)
-	
+				#alert and start ipython upon success
+				comand = 'gnome-terminal -x bash -c "./odds_crawler.py"'
+				os.system(comand)
+				
+				#~ break
 
+			mins = 30
+			secs = mins*60
+			print 'sleeping for %d seconds' % secs
+			bar = progressbar.ProgressBar()
+			for sec in bar(xrange(secs)):
+				time.sleep(1)
+			count+=1
+		
 
