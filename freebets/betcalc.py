83b0f9fcb7da6a51313f4fb9f86a051579b23838
Mirko <mirko.scherer@gmx.net>
2016-08-19 18:34:09 -0300

    update betcalc

--- freebets/betcalc.py
+++ freebets/betcalc.py
@@ -5,6 +5,8 @@
 from IPython import embed
 
 import numpy as np
+import matplotlib.pyplot as mp
+import matplotlib.gridspec as gridspec
 from my.tools import closeTo 
 
 np.set_printoptions(precision=3, threshold=50, linewidth=100)		
@@ -59,11 +61,11 @@ class Bets():
 		
 		return out
 
-	def get_stakes_50(self,liability=50):
+	def get_stakes_50(self,liability=50,verbose=True):
 		
 		stake = self.get_stake(liability,verbose=False)
 		stake = np.round(stake*self.exchange_rate,0) /self.exchange_rate
-		liability = self.get_liability(stake,verbose=True)
+		liability = self.get_liability(stake,verbose=verbose)
 		
 
 			
@@ -71,8 +73,50 @@ def main():
 	print 'here starts main program'
 
 	b = Bets()
-	b.set_rates(2.87,2.86)
-	b.get_stakes_50()
+	diffs = np.linspace(-0.1,0.1,21)
+	
+	brates,lrates,profits = [],[],[]
+	brate=1.01
+	while brate<3:
+		
+		for d in diffs:
+			
+			lrate = brate+d
+			
+			if lrate<1.01: lrate=1.01
+			
+			b.set_rates(brate,lrate)
+			b.get_stake(50,verbose=False)
+			profit = b.get_profit_laywin()
+			
+			if profit>0: 
+				print brate,lrate,profit
+			
+			brates.append(brate)
+			lrates.append(lrate)
+			profits.append(profit)
+		
+		brate+=.1
+	
+	diffs = [ brates[i]-lrates[i] for i in xrange(len(brates)) ]
+	array = np.array(zip(brates,lrates,diffs,profits),dtype=[('brate',float),('lrate',float),('diff',float),('profit',float)])
+	
+	mp.figure(num=1, figsize=(8, 6), dpi=80)
+	mp.clf()
+	gs1 = gridspec.GridSpec(1,1)
+	ax1 = mp.subplot(gs1[0, 0])
+	
+	ax1.plot(array['brate'],array['profit'],marker='.',ls='',c='r',label='')
+	
+	ax1.axhline(0, c='#B3B3B3', ls='--')
+	
+	#~ ax1.set_title(r'cashflow globalfit ($\sigma=%d$)' % self.sigma)
+	#~ ax1.set_xlabel(r'years', size='large')
+	#~ ax1.set_ylabel(r'cashflow / k', size='large')
+	#~ ax1.set_xlim(left=np.min(x)-.5,right=np.max(x)+.5)
+	#~ mp.savefig('cf_globalfit_%dsigma.pdf' % self.sigma)
+	mp.show()	
+	
 		
 if __name__ == "__main__":
     main()		
